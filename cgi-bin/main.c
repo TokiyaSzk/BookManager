f59098708eb07b01d99d588ce9d404e404ab0298
tokiya <e2544733@outlook.com>
2024-05-16 17:22:46 +0800

    1.2-ver

--- cgi-bin/main.c
+++ cgi-bin/main.c
@@ -1,15 +1,7 @@
-//
-//  main.c
-//  test
-//
-//  Created by Tokya_mac on 2024/5/16.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Content-Type: text/plain");
+int main(void) {
+    printf("Content-Type: text/plain\n\n");
     printf("Hello, World!\n");
     return 0;
 }
