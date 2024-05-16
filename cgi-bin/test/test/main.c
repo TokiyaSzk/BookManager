a2addfcfa8e1312d218d0633095628a6a55523e6
tokiya <e2544733@outlook.com>
2024-05-16 17:02:23 +0800

    0.8-ver

---
+++ cgi-bin/test/test/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  test
+//
+//  Created by Tokya_mac on 2024/5/16.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
